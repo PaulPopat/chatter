7db758288adbf83ed4b9ef911732038efc592c1f
Paul Popat <paul@popatux.co.uk>
2024-02-07 12:05:19 +0000

    Getting started

---
+++ src/AwsInfrastructure/EffuseCoreStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace Effuse
+{
+  public class EffuseCoreStack : Stack
+  {
+    internal EffuseCoreStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+    {
+      // The code that defines your stack goes here
+    }
+  }
+}
---
+++ src/AwsInfrastructure/Program.cs
@@ -0,0 +1,16 @@
+﻿using Amazon.CDK;
+
+namespace Effuse
+{
+  sealed class Program
+  {
+    public static void Main(string[] args)
+    {
+      var app = new App();
+      new EffuseCoreStack(app, "core-stack", new StackProps
+      {
+      });
+      app.Synth();
+    }
+  }
+}
