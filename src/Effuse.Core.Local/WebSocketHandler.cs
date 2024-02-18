ed463fc90afaa6ec0e7ce50cb22c0f5b3a3b530e
Paul Popat <paul@popatux.co.uk>
2024-02-18 21:10:18 +0000

    Better web sockets

---
+++ src/Effuse.Core.Local/WebSocketHandler.cs
@@ -0,0 +1,48 @@
+﻿using Effuse.Core.Handlers.Contracts;
+using WebSocketSharp.Server;
+using Effuse.Core.Utilities;
+using WebSocketSharp;
+using System.Text.Json;
+
+namespace Effuse.Core.Local;
+
+public class WebSocketHandler : WebSocketBehavior
+{
+  private readonly IWebSocketHandler handler;
+
+  public WebSocketHandler(IWebSocketHandler handler)
+  {
+    this.handler = handler;
+  }
+
+  protected override void OnOpen()
+  {
+    base.OnOpen();
+    this.handler.OnConnect(new HandlerProps(
+      this.Context.RequestUri.AbsolutePath,
+      "GET",
+      this.ID,
+      new Dictionary<string, string>(),
+      this.Context.RequestUri.GetQueryString(),
+      this.Context.Headers.ToDictionary().ToLowerCaseKeys()));
+  }
+
+  protected override async void OnMessage(MessageEventArgs e)
+  {
+    base.OnMessage(e);
+    var response = await this.handler.OnMessage(this.ID, e.Data);
+    this.Send(JsonSerializer.Serialize(response));
+  }
+
+  protected override void OnClose(CloseEventArgs e)
+  {
+    base.OnClose(e);
+    this.handler.OnClose(this.ID);
+  }
+
+  protected override void OnError(WebSocketSharp.ErrorEventArgs e)
+  {
+    base.OnError(e);
+    this.handler.OnClose(this.ID);
+  }
+}
--- src/Effuse.Core.Local/WebSocketServer.cs
+++ src/Effuse.Core.Local/WebSocketServer.cs
@@ -8,49 +8,19 @@ using BaseServer = WebSocketSharp.Server.WebSocketServer;
 
 namespace Effuse.Core.Local;
 
-public class WebSocketServer<THandler> : WebSocketBehavior
-  where THandler : IWebSocketHandler
+public class WebSocketServer
 {
-  private static readonly Lazy<BaseServer> Server = new(() => new BaseServer(3001));
-
-  private readonly IWebSocketHandler handler;
-  private readonly string url;
-
-  public WebSocketServer(UnityContainer container, string url)
+  public WebSocketServer(int port, UnityContainer container, IEnumerable<Route> routes)
   {
-    this.handler = container.Resolve<THandler>();
-    this.url = url;
-    Server.Value.AddWebSocketService<WebSocketServer<THandler>>(url, () => this);
-  }
+    var server = new BaseServer(port);
 
-  protected override void OnOpen()
-  {
-    base.OnOpen();
-    this.handler.OnConnect(new HandlerProps(
-      this.url,
-      "GET",
-      this.ID,
-      new Dictionary<string, string>(),
-      this.Context.RequestUri.GetQueryString(),
-      this.Context.Headers.ToDictionary().ToLowerCaseKeys()));
-  }
+    foreach (var route in routes)
+    {
+      server.AddWebSocketService(
+        route.Path,
+        () => new WebSocketHandler((IWebSocketHandler)container.Resolve(route.Handler)));
+    }
 
-  protected override async void OnMessage(MessageEventArgs e)
-  {
-    base.OnMessage(e);
-    var response = await this.handler.OnMessage(this.ID, e.Data);
-    this.Send(JsonSerializer.Serialize(response));
-  }
-
-  protected override void OnClose(CloseEventArgs e)
-  {
-    base.OnClose(e);
-    this.handler.OnClose(this.ID);
-  }
-
-  protected override void OnError(WebSocketSharp.ErrorEventArgs e)
-  {
-    base.OnError(e);
-    this.handler.OnClose(this.ID);
+    server.Start();
   }
 }
