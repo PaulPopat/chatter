733a2750296e539f449fdf19e7ee2281f45bee02
Paul Popat <paul@popatux.co.uk>
2024-02-19 08:49:02 +0000

    Cleaned up contracts

---
+++ src/Effuse.Core.Integration/Contracts/ISubscriptions.cs
@@ -0,0 +1,8 @@
+﻿namespace Effuse.Core.Integration;
+
+public interface ISubscriptions
+{
+  Task Subscribe(string channel, string connectionId);
+
+  Task Broadcast(string channel, object message);
+}
