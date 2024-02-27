d717f5287694b7ed5157ad9ebffd86128ee332a7
Paul Popat <paul@popatux.co.uk>
2024-02-27 16:08:07 +0000

    Fixed security holes

---
+++ docker-scripts/start-server.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+if [ ! -f /data/private_key.pem ]; then
+  openssl req -new -newkey rsa:4096 -nodes -keyout signing_key.key -out public_key.csr -subj "/C=UK/CN=local"
+  openssl x509 -req -sha512 -days 365 -in public_key.csr -signkey signing_key.key -out private_key.pem
+
+  cp private_key.pem /data/private_key.pem
+  cp signing_key.key /data/signing_key.key
+fi
+
+./Effuse.Server.Local
