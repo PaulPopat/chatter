ab28833fbbde8753b2aa5269c7557c96878248c0
Paul Popat <paul@popatux.co.uk>
2024-03-19 11:54:44 +0000

    Better favicons

--- user-interface/webpack.config.js
+++ user-interface/webpack.config.js
@@ -3,6 +3,8 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const VirtualModulesPlugin = require("webpack-virtual-modules");
 const path = require("path");
+const { favicons } = require("favicons");
+const { Compilation, sources } = require("webpack");
 
 const isProd = process.env.PROD === "true";
 
@@ -10,51 +12,57 @@ const config = {
   SSO_BASE: process.env.SSO_BASE_URL,
 };
 
-/** @type {import('webpack').Configuration} */
-module.exports = {
-  entry: path.resolve(__dirname, "src", "index.ts"),
-  output: {
-    path: path.join(__dirname, "dist"),
-    filename: '[hash].js',
-    chunkFilename: '[chunkhash].js',
-  },
-  devtool: "source-map",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-      {
-        test: /\.svg$/,
-        loader: "svg-inline-loader",
-      },
-    ],
-  },
-  resolve: {
-    alias: {
-      "react-native$": "react-native-web",
+/** @type {() => Promise<import('webpack').Configuration>} */
+module.exports = async () => {
+  const response = await favicons(
+    path.resolve(__dirname, "../asset-src/app-icon.png")
+  );
+
+  return {
+    entry: path.resolve(__dirname, "src", "index.ts"),
+    output: {
+      path: path.join(__dirname, "dist"),
+      filename: "[hash].js",
+      chunkFilename: "[chunkhash].js",
     },
-    extensions: [".tsx", ".ts", ".js"],
-  },
-  plugins: [
-    new MiniCssExtractPlugin(),
-    new HtmlWebpackPlugin({
-      title: "Effuse",
-      inject: false,
-      meta: {
-        viewport:
-          "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no",
+    devtool: "source-map",
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, "css-loader"],
+        },
+        {
+          test: /\.svg$/,
+          loader: "svg-inline-loader",
+        },
+      ],
+    },
+    resolve: {
+      alias: {
+        "react-native$": "react-native-web",
       },
-      templateContent: ({ htmlWebpackPlugin }) => `
+      extensions: [".tsx", ".ts", ".js"],
+    },
+    plugins: [
+      new MiniCssExtractPlugin(),
+      new HtmlWebpackPlugin({
+        title: "Effuse",
+        inject: false,
+        meta: {
+          viewport:
+            "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no",
+        },
+        templateContent: ({ htmlWebpackPlugin }) => `
         <html>
           <head>
             ${htmlWebpackPlugin.tags.headTags}
+            ${response.html.join("")}
             <style>
               html,
               body {
@@ -75,23 +83,47 @@ module.exports = {
           </body>
         </html>
       `,
-    }),
-    new VirtualModulesPlugin({
-      "node_modules/@effuse/config.js": `module.exports = ${JSON.stringify(
-        config
-      )};`,
-    }),
-  ],
-  mode: isProd ? "production" : "development",
-  ...(!isProd
-    ? {
-        devServer: {
-          port: 3001,
-          historyApiFallback: {
-            index: "index.html",
+      }),
+      new VirtualModulesPlugin({
+        "node_modules/@effuse/config.js": `module.exports = ${JSON.stringify(
+          config
+        )};`,
+      }),
+      async (compiler) => {
+        compiler.hooks.thisCompilation.tap("Copy Icons", (compilation) => {
+          compilation.hooks.processAssets.tap(
+            {
+              name: "Copy Icons",
+              stage: Compilation.PROCESS_ASSETS_STAGE_SUMMARIZE,
+            },
+            async (assets) => {
+              for (const file of response.files)
+                compilation.emitAsset(
+                  file.name,
+                  new sources.RawSource(file.contents)
+                );
+
+              for (const image of response.images)
+                compilation.emitAsset(
+                  image.name,
+                  new sources.RawSource(image.contents)
+                );
+            }
+          );
+        });
+      },
+    ],
+    mode: isProd ? "production" : "development",
+    ...(!isProd
+      ? {
+          devServer: {
+            port: 3001,
+            historyApiFallback: {
+              index: "index.html",
+            },
           },
-        },
-      }
-    : {}),
-  watch: !isProd,
+        }
+      : {}),
+    watch: !isProd,
+  };
 };
